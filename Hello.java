9930ce1cdf357ae4e2170371fb91817ce65a7585
Avishek Das <dasavishek669@gmail.com>
2025-07-09 08:31:09 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+	public static void main(String args[]){
+		System.out.println("Our First java program");
+	System.out.println("Currently we are learning web technologies");
+	}
+}
